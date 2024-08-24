e2796f06ae62c0affa4a63b34597e9ff5b75e0aa
mohammad rafi shaik <rafitest444@hotmail.com>
2024-08-24 15:49:55 +0300

    first commit

---
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message(){
+		return "Welcome my DevOps World -hey there howdy2 - all good - no issues on this cicd pipeline- ramesh initial check";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+	}
+
+}
